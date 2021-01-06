8ad70008bd0bed3a50b1cf5b3fb566796fb1c8b4
LumaKernel <LumaKernel@users.noreply.github.com>
2021-01-06 17:31:01 +0000

    deploy: 60479489faa82d9070859fd2640728a4b9c9bef5

--- _next/static/2yMjmF4xtvT7paEkT92q2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F89a47b2c8eed51cb4c3b.css","static\u002Fchunks\u002Fpages\u002Findex-3662a7ccc77bf08f426a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-72e0b0f810dae4ca3a25.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
