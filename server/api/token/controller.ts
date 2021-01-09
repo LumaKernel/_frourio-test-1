c63f1ae825d5045fcfae9ba9eb2207ea3c5d0bb5
Luma <world@luma.email>
2021-01-09 14:32:52 +0900

    feat: 01/09/21 14:32:52

--- server/api/token/controller.ts
+++ server/api/token/controller.ts
@@ -1,9 +1,25 @@
 import { defineController } from './$relay'
 import { validateUser } from '$/service/user'
+import crypto from 'crypto'
+import { FAIL_PASS } from '$/service/envValues'
 
 export default defineController((fastify) => ({
-  post: ({ body }) =>
-    validateUser(body.id, body.pass)
-      ? { status: 201, body: { token: fastify.jwt.sign({ id: body.id }) } }
-      : { status: 401 }
+  post: ({ body }) => {
+    const valid = validateUser(body.id, body.pass)
+
+    if (FAIL_PASS !== null) {
+      if (
+        body.id === 'fail' &&
+        crypto.timingSafeEqual(Buffer.from(body.pass), Buffer.from(FAIL_PASS))
+      ) {
+        throw new Error('Unexpected and unhanded error!: ' + new Date())
+      }
+    }
+
+    if (valid) {
+      return { status: 201, body: { token: fastify.jwt.sign({ id: body.id }) } }
+    } else {
+      return { status: 401 }
+    }
+  }
 }))
--- server/service/envValues.ts
+++ server/service/envValues.ts
@@ -8,5 +8,14 @@ const USER_PASS = process.env.USER_PASS ?? ''
 const SERVER_PORT = +(process.env.SERVER_PORT ?? '8080')
 const BASE_PATH = process.env.BASE_PATH ?? ''
 const API_ORIGIN = process.env.API_ORIGIN ?? ''
+const FAIL_PASS = process.env.FAIL_PASS || null
 
-export { JWT_SECRET, USER_ID, USER_PASS, SERVER_PORT, BASE_PATH, API_ORIGIN }
+export {
+  JWT_SECRET,
+  USER_ID,
+  USER_PASS,
+  SERVER_PORT,
+  BASE_PATH,
+  API_ORIGIN,
+  FAIL_PASS
+}
