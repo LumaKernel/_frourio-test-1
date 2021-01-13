56f7e80c7d9264678c11b2863f4b410979bad921
Luma <world@luma.email>
2021-01-14 01:04:48 +0900

    fix: specify migration schema

--- server/entrypoints/lambda_migration.ts
+++ server/entrypoints/lambda_migration.ts
@@ -3,8 +3,14 @@ import { MigrateDeploy } from '@prisma/migrate'
 
 // Prisma cli needs network access and write access to node_modules/...
 // Then we cannot use that here.
+
+// Assume this executed on project root.
 const handler: Handler = async () => {
-  await MigrateDeploy.new().parse(['--preview-feature'])
+  await MigrateDeploy.new().parse([
+    '--schema',
+    './server/prisma/schema.prisma',
+    '--preview-feature'
+  ])
   return 'migration_ok'
 }
 
