387757c850212b71ba46b4683f1a9b5dca491c50
Luma <world@luma.email>
2021-01-13 18:58:57 +0900

    ci: fix

--- server/entrypoints/lambda_migration.ts
+++ server/entrypoints/lambda_migration.ts
@@ -2,5 +2,5 @@ import { Handler } from 'aws-lambda'
 import { execFileSync } from 'child_process'
 
 export const handler: Handler = () => {
-  execFileSync('npm', ['run', 'migrate:up'])
+  execFileSync('npm', ['run', 'migrate:deploy'])
 }
