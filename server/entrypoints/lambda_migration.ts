39c0c1580ee174e1af3ef665a2eabcbf5490f7f2
Luma <world@luma.email>
2021-01-14 18:41:13 +0900

    chore: debug

--- server/entrypoints/lambda_migration.ts
+++ server/entrypoints/lambda_migration.ts
@@ -2,6 +2,10 @@ import type { Handler } from 'aws-lambda'
 import { execFileSync } from 'child_process'
 
 const handler: Handler = async () => {
+  console.info('here1!')
+  console.info(process.env)
+  console.info('here2!')
+
   // Assume this executed on project root.
   execFileSync('npm', ['run', 'migrate:deploy'])
 
